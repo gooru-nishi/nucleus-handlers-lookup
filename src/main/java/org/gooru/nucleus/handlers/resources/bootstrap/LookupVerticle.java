58c8328e84e262ba2629986934bbcc2d4365f6ef
Ashish Mishra <ashish@gooru.org>
2015-12-25 12:11:55 +0530

    Initial repo creation

---
+++ src/main/java/org/gooru/nucleus/handlers/resources/bootstrap/LookupVerticle.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.resources.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class LookupVerticle extends AbstractVerticle {
+}
