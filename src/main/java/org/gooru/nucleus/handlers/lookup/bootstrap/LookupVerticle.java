251550197c7544f7667eb79b60519b31fac34300
Ashish Mishra <ashish@gooru.org>
2015-12-26 14:02:30 +0530

    Bootstrapping the infrastructure

---
+++ src/main/java/org/gooru/nucleus/handlers/lookup/bootstrap/LookupVerticle.java
@@ -0,0 +1,95 @@
+package org.gooru.nucleus.handlers.lookup.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+import org.gooru.nucleus.handlers.lookup.bootstrap.shutdown.Finalizer;
+import org.gooru.nucleus.handlers.lookup.bootstrap.shutdown.Finalizers;
+import org.gooru.nucleus.handlers.lookup.bootstrap.startup.Initializer;
+import org.gooru.nucleus.handlers.lookup.bootstrap.startup.Initializers;
+import org.gooru.nucleus.handlers.lookup.constants.MessageConstants;
+import org.gooru.nucleus.handlers.lookup.constants.MessagebusEndpoints;
+import org.gooru.nucleus.handlers.lookup.processors.ProcessorBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class LookupVerticle extends AbstractVerticle {
+
+  static final Logger LOGGER = LoggerFactory.getLogger(LookupVerticle.class);
+
+  @Override
+  public void start(Future<Void> voidFuture) throws Exception {
+
+    vertx.executeBlocking(blockingFuture -> {
+      startApplication();
+    }, future -> {
+      if (future.succeeded()) {
+        voidFuture.complete();
+      } else {
+        voidFuture.fail("Not able to initialize the Lookup machinery properly");
+      }
+    });
+
+    EventBus eb = vertx.eventBus();
+
+    eb.consumer(MessagebusEndpoints.MBEP_LOOKUP, message -> {
+
+      LOGGER.debug("Received message: " + message.body());
+
+      vertx.executeBlocking(future -> {
+        JsonObject result = new ProcessorBuilder(message).build().process();
+        future.complete(result);
+      }, res -> {
+        JsonObject result = (JsonObject) res.result();
+        DeliveryOptions options = new DeliveryOptions().addHeader(MessageConstants.MSG_OP_STATUS, result.getString(MessageConstants.MSG_OP_STATUS));
+        message.reply(result.getJsonObject(MessageConstants.RESP_CONTAINER_MBUS), options);
+
+        JsonObject eventData = result.getJsonObject(MessageConstants.RESP_CONTAINER_EVENT);
+        if (eventData != null) {
+          eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
+        }
+
+      });
+
+
+    }).completionHandler(result -> {
+      if (result.succeeded()) {
+        LOGGER.info("Lookup end point ready to listen");
+      } else {
+        LOGGER.error("Error registering the lookup handler. Halting the Lookup machinery");
+        Runtime.getRuntime().halt(1);
+      }
+    });
+  }
+
+  @Override
+  public void stop() throws Exception {
+    shutDownApplication();
+    super.stop();
+  }
+
+  private void startApplication() {
+    Initializers initializers = new Initializers();
+    try {
+      for (Initializer initializer : initializers) {
+        initializer.initializeComponent(vertx, config());
+      }
+    } catch(IllegalStateException ie) {
+      LOGGER.error("Error initializing application", ie);
+      Runtime.getRuntime().halt(1);
+    }
+  }
+
+  private void shutDownApplication() {
+    Finalizers finalizers = new Finalizers();
+    for (Finalizer finalizer : finalizers ) {
+      finalizer.finalizeComponent();
+    }
+
+  }
+}
---
+++ src/main/java/org/gooru/nucleus/handlers/lookup/constants/MessagebusEndpoints.java
@@ -0,0 +1,11 @@
+package org.gooru.nucleus.handlers.lookup.constants;
+
+public class MessagebusEndpoints {
+  /*
+   * Any change here in end points should be done in the gateway side as well, as both sender and receiver should be in sync
+   */
+  public static final String MBEP_LOOKUP = "org.gooru.nucleus.message.bus.lookup";
+  public static final String MBEP_EVENT = "org.gooru.nucleus.message.bus.publisher.event";
+  
+
+}
